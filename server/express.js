ac94f57554aa5c4463bc9a98865d9dce1f0203b1
Ushindi Bienvenu <jeanbienvenusb@gmail.com>
2022-01-24 19:58:17 +0000

    Integrate user auth and protected routes

--- server/express.js
+++ server/express.js
@@ -1,21 +1,28 @@
 import express from 'express';
-import userRoutes from './routes/user.routes'
+import userRoutes from './routes/user.routes';
+import authRoutes from './routes/auth.routes';
 import Template from './../template';
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const compress = require('compression');
 const helmet = require('helmet');
 const cors = require('cors');
-const app = express()
+const app = express();
 /*... configure express ... */
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cookieParser())
-app.use(compress())
-app.use(helmet())
-app.use(cors())
-app.use('/', userRoutes)
-app.get('/', (req, res) => {
-res.status(200).send(Template())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(compress());
+app.use(helmet());
+app.use(cors());
+app.use('/', userRoutes);
+app.use('/', authRoutes);
+app.use((err, req, res, next) => {
+if (err.name === 'UnauthorizedError') {
+res.status(401).json({"error" : err.name + ": " + err.message})
+}else if (err) {
+res.status(400).json({"error" : err.name + ": " + err.message})
+console.log(err)
+}
 })
 export default app
